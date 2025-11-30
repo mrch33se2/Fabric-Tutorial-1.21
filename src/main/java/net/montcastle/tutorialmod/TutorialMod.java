0a0ff62074252cd83cb15788431e12da839dd515
mrch33se2 <geddings.hempel@gmail.com>
2025-11-30 14:25:03 -0600

    Initial Commit

--- src/main/java/net/montcastle/tutorialmod/TutorialMod.java
+++ src/main/java/net/montcastle/tutorialmod/TutorialMod.java
@@ -2,6 +2,9 @@ package net.montcastle.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.montcastle.tutorialmod.block.ModBlocks;
+import net.montcastle.tutorialmod.item.ModItemGroups;
+import net.montcastle.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +14,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
