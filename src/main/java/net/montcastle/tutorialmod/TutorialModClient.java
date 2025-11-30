c7b93905f760126aad1030b18d62ffb4f69c9835
mrch33se2 <geddings.hempel@gmail.com>
2025-11-29 18:08:28 -0600

    Initial Commit

---
+++ src/main/java/net/montcastle/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.montcastle.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
