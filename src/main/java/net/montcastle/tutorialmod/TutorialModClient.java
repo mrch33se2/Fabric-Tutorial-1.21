e03e41c80c7f0049c29362c1986ed935f93f4d5b
mrch33se2 <geddings.hempel@gmail.com>
2025-12-05 09:01:47 -0600

    Fixed Transparency For Doors And Trapdoors And Names For Button And Pressure Plate

--- src/main/java/net/montcastle/tutorialmod/TutorialModClient.java
+++ src/main/java/net/montcastle/tutorialmod/TutorialModClient.java
@@ -1,10 +1,14 @@
 package net.montcastle.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.montcastle.tutorialmod.block.ModBlocks;
 
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
     }
 }
