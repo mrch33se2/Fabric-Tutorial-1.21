f70506dff77e4a9921b8eae401a30e4af4a7c797
mrch33se2 <geddings.hempel@gmail.com>
2025-12-09 08:09:06 -0600

    Pink Garnet Armor

--- src/main/java/net/montcastle/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/montcastle/tutorialmod/item/ModItemGroups.java
@@ -32,6 +32,11 @@ public class ModItemGroups {
                         entries.add(ModItems.PINK_GARNET_HOE);
 
                         entries.add(ModItems.PINK_GARNET_HAMMER);
+
+                        entries.add(ModItems.PINK_GARNET_HELMET);
+                        entries.add(ModItems.PINK_GARNET_CHESTPLATE);
+                        entries.add(ModItems.PINK_GARNET_LEGGINGS);
+                        entries.add(ModItems.PINK_GARNET_BOOTS);
                     }).build());
 
     public static final ItemGroup CUSTOM_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
